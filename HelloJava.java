4d72df408bce0d3772b589ad9f9e18c45e97ce47
Garret Thompson <gthompson@rsglab.com>
2019-02-28 10:51:52 -0500

    Test Java DNS lookups

--- HelloJava.java
+++ HelloJava.java
@@ -1,7 +1,25 @@
 package examples;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class HelloJava {
-	public static void main(String... args) {
-		System.out.println("Hello from the container.");
-	}
+  public static void main(String... args) throws InterruptedException {
+    System.out.println("Hello from the container.");
+    if (args.length == 0) {
+      return;
+    }
+
+    String hostname = args[0];
+    while (true) {
+      System.out.printf("==> Looking up %s%n", hostname);
+      try {
+        InetAddress address = InetAddress.getByName(hostname);
+        System.out.println(address);
+      } catch (UnknownHostException e) {
+        System.err.println("ERROR: failed host lookup");
+      }
+      Thread.sleep(2000);
+    }
+  }
 }
